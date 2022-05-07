ee93a3bb42d0e40e42784caac22e2b72631fc50f
Vik-440 <v.paradyuk@gmail.com>
2022-05-07 21:30:11 +0300

    19

--- work-pom/demo1/app_flask.py
+++ work-pom/demo1/app_flask.py
@@ -4,11 +4,14 @@ from _1_main_json import return_data_from_mainpage
 from _2_new_order_json import return_data_from_new_order, return_data_from_new_order_post
 from _3_material_json import return_data_from_material, return_data_from_material_one, \
     return_data_from_material_change, return_data_from_material_new, return_data_from_material_change_full
-
+from _4_finance_json import return_data_from_finance, return_data_from_payment,return_data_from_outlay, \
+    return_data_from_payment_search, return_data_from_outlay_search, return_data_from_payment_change, \
+    return_data_from_outlay_change, return_data_from_payment_id_order, return_data_from_payment_stat, \
+    return_data_from_payment_balans
 
 app =   Flask(__name__)
 app.config['JSON_AS_ASCII'] = False
-  
+#########################################################################################  
 @app.route('/')
 def return_data_from_flask ():
 
@@ -21,7 +24,7 @@ def return_data_from_flask ():
     return jsonify(info) # returning a JSON response
 
 
-
+#########################################################################################
 @app.route('/mainpage', methods=['GET', 'POST'])
 def mainpage ():
     if request.method == 'POST':
@@ -36,7 +39,7 @@ def mainpage ():
     else:
         data = return_data_from_mainpage(0,0)
         return data
-
+#########################################################################################
 @app.route('/new_order', methods=['GET', 'POST'])
 def new_order ():
     if request.method == 'POST':
@@ -47,7 +50,7 @@ def new_order ():
         data=return_data_from_new_order()
 
         return data
-
+#########################################################################################
 @app.route('/material', methods=['GET', 'POST'])
 def material ():
     if request.method == 'POST':
@@ -66,15 +69,38 @@ def material ():
             data=return_data_from_material_change_full(request.data)
         else:
             data={"запит ":"не вірний"}
-
-
         # data={"testdata" : "Test-POST-OK"}
         return data
     else:
-        data=return_data_from_material(0)
-        return data
-
-
-
+        return(return_data_from_material(0))
+#########################################################################################
+@app.route('/finance', methods=['GET', 'POST'])
+def finance ():
+    if request.method == 'POST':
+        request.data = request.get_json()
+        if type(request.data) is list:
+            if 'payment_group' in request.data[0]:
+                return (return_data_from_payment(request.data))
+            if 'outlay_group' in request.data[0]:
+                return (return_data_from_outlay(request.data))
+        elif type(request.data) is dict:
+            if 'payment_search' in request.data:
+                return (return_data_from_payment_search(request.data))
+            if 'outlay_search' in request.data:
+                return (return_data_from_outlay_search(request.data))
+            if 'id_payment' in request.data:
+                return (return_data_from_payment_change(request.data))
+            if 'id_outlay' in request.data:
+                return (return_data_from_outlay_change(request.data))
+            if 'id_order' in request.data:
+                return (return_data_from_payment_id_order(request.data))
+            if 'stat' in request.data:
+                return (return_data_from_payment_stat(request.data))    
+            if 'balans' in request.data:
+                return (return_data_from_payment_balans(request.data))
+        return({"testdata" : "Test-POST-NOT-OK"})
+    else:
+        return(return_data_from_finance(0))
+#########################################################################################
 if __name__=='__main__':
     app.run(debug=True)
