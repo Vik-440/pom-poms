3c829273cace5f3462a575621edeb8555c0b3efe
DanaDov <danadovzh16@gmail.com>
2022-07-06 10:42:50 +0300

    add some new fix

--- frontend/src/app/finances/finances.component.ts
+++ frontend/src/app/finances/finances.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
 import { FinancesPageService } from '../services/finances.service';
 
 @Component({
@@ -8,21 +9,38 @@ import { FinancesPageService } from '../services/finances.service';
 })
 export class FinancesComponent implements OnInit {
 
-  constructor(private service: FinancesPageService) { }
+  constructor(private service: FinancesPageService, private fb: FormBuilder) { }
 
   items = [];
-  methods;
+  metodPayment;
+  paymentFrom;
+  spendingForm;
   mainItems;
+  periods: string[];
   outlayData;
+  todayYear = new Date().getFullYear();
   ngOnInit(): void {
+    this.paymentFrom = this.fb.group({
+      metod: '',
+      dateFrom: '',
+      dateTo: '',
+      period: '',
+      numberOrder: ''
+    });
+    this.spendingForm = this.fb.group({
+      dateFrom: '',
+      dateTo: ''
+    })
     this.service.getFinances().subscribe((data: any) => {
       const copyData = data.slice(0);
-      this.methods = data[0];
+      this.metodPayment = data[0].metod_payment;
       this.outlayData = data[data.length - 1];
       this.mainItems = copyData.slice(1, data.length - 1);
-      console.log(this.mainItems, copyData);
-      
     })
+    this.periods = ['день', 'тиждень', 'місяць', 'квартал', 'рік']
+  }
+
+  click() {
   }
 
 }
