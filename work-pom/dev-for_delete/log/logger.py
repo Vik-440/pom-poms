29f1a32efb9190cdf0e2a02d7e4087ae0cf0e231
Vik-440 <v.paradyuk@gmail.com>
2023-01-21 16:15:07 +0200

    delete directory with dev code

--- work-pom/dev-for_delete/log/logger.py
+++
@@ -1,9 +0,0 @@
-import logging
-
-formatter = logging.Formatter('%(asctime)s:%(levelname)s:%(message)s')
-logger = logging.getLogger(__name__)
-logger.setLevel(logging.DEBUG)
-fh_file = logging.FileHandler('pom-poms.log')
-fh_file.setLevel(logging.INFO)
-fh_file.setFormatter(formatter)
-logger.addHandler(fh_file)
