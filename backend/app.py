515bcc081f5a19bf41202dfa3b5488e37b7adcea
Vik-440 <v.paradyuk@gmail.com>
2023-01-23 22:01:20 +0200

    fast fix app

--- backend/app.py
+++ backend/app.py
@@ -5,11 +5,13 @@
 #     app = create_app()
 #     app.run(load_dotenv=True, host='127.0.0.2')
 
-
 from flask import Flask
+
 app = Flask(__name__)
 
-@app.route("/")
-def home():
-    return "Hello, Flask!"
+
+@app.route('/')
+def hello():
+    return 'Hello, World!'
+
     
