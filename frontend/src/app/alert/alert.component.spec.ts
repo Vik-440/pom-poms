e706be375388b32a2d39d1022b720656a196bc6d
DanaDov <danadovzh16@gmail.com>
2023-04-28 17:10:12 +0300

    fix text for alert

--- frontend/src/app/alert/alert.component.spec.ts
+++ frontend/src/app/alert/alert.component.spec.ts
@@ -1,13 +1,27 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { AlertComponent } from './alert.component';
+import { By } from '@angular/platform-browser';
 
 describe('AlertComponent', () => {
   let component: AlertComponent;
   let fixture: ComponentFixture<AlertComponent>;
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [ AlertComponent ],
+    });
+    fixture = TestBed.createComponent(AlertComponent);
+    component = fixture.componentInstance; // BannerComponent test instance
+  });
 
   it('should create an instance', () => {
-    const component = new AlertComponent();
     expect(component).toBeTruthy();
   });
+
+  it('should emit close', () => {
+    jest.spyOn(component.closed, 'emit');
+    const closeButton = fixture.debugElement.query(By.css('.close')).nativeElement;
+    closeButton.click();
+    expect(component.closed.emit).toHaveBeenCalled();
+  })
 });
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -6,7 +6,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { NgSelectModule } from '@ng-select/ng-select';
-// import { TooltipModule, TooltipOptions } from '@teamhive/ngx-tooltip';
 import { ClickOutsideModule } from 'ng-click-outside';
 import { DatepickerModule } from 'ng2-datepicker';
 import { NgxMaskModule } from 'ngx-mask';
