4ba3ea99d41408ea0086f9b67de78050f44cceef
DanaDov <danadovzh16@gmail.com>
2023-05-02 17:28:49 +0300

    add materials url

--- frontend/src/app/reserve/reserve.component.ts
+++ frontend/src/app/reserve/reserve.component.ts
@@ -1,220 +1,235 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit, ViewChild } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { switchMap } from 'rxjs';
 import { MaterialPageService } from '../services/materials.service';
 
 @Component({
-    selector: 'app-reserve',
-    templateUrl: './reserve.component.html',
-    styleUrls: ['./reserve.component.sass'],
+  selector: 'app-reserve',
+  templateUrl: './reserve.component.html',
+  styleUrls: ['./reserve.component.sass'],
 })
 export class ReserveComponent implements OnInit {
-    constructor(private _serviceMaterial: MaterialPageService, private _fb: FormBuilder) {}
-    materialFilter = [];
-    reverseItemData: FormGroup;
-    reserveItems;
-    reverseItemsCorrect = this._fb.array([]);
-    idEdit = null;
-    isNewMaterial = false;
-    idChange = [];
-    isHideOk = true;
-    filterMaterial = null;
-    isShowSpinner = false;
-    alert = {
-        type: '',
-        message: '',
-        isShow: false,
+  constructor(private _serviceMaterial: MaterialPageService, private _fb: FormBuilder) {}
+  materialFilter = [];
+  reverseItemData: FormGroup;
+  reserveItems;
+  reverseItemsCorrect = this._fb.array([]);
+  idEdit = null;
+  isNewMaterial = true;
+  idChange = [];
+  isHideOk = true;
+  filterMaterial = null;
+  isShowSpinner = false;
+  alert = {
+    type: '',
+    message: '',
+    isShow: false,
+  };
+  @ViewChild('blockEdit', { static: true }) blockEdit;
+  @HostListener('window:scroll', ['$event']) // for window scroll events
+  onScroll() {
+    this.blockEdit.nativeElement.setAttribute('style', `top: ${window.scrollY}px`);
+  }
+
+  ngOnInit(): void {
+    this.getMaterialList();
+    this.materialFilter = [
+      { value: 'all', name: 'всі матеріали' },
+      { value: null, name: 'матеріали в наявності' },
+    ];
+    this.reverseItemData = this._fb.group({
+      id_material: '',
+      name: ['', Validators.required],
+      width: [null, Validators.required],
+      thickness: [null, Validators.required],
+      manufacturer: ['', Validators.required],
+      spool_weight: null,
+      weight_10m: [null, Validators.required],
+      reserve: [null, Validators.required],
+      spool_qty: [null, Validators.required],
+      weight: [null, Validators.required],
+      comment: '',
+    });
+  }
+
+  getMaterialList() {
+    this._serviceMaterial.getListMaterial().subscribe(
+      (data: any) => {
+        this.prepareListMaterial(data);
+      },
+      () => {}
+    );
+  }
+
+  prepareListMaterial(data) {
+    this.reserveItems = data.sort((a, b) => a.name - b.name);
+    this.reserveItems.map(() => {
+      this.reverseItemsCorrect.push(
+        this._fb.group({
+          edit_spool_qty: null,
+          edit_weight: null,
+        })
+      );
+    });
+  }
+
+  editItem(id) {
+    this._serviceMaterial.getFullInfoMaterial(id).subscribe((data: any) => {
+      this.idEdit = id;
+      this.isNewMaterial = false;
+      this.reverseItemData.patchValue({
+        id_material: data.id_material,
+        name: data.name,
+        width: data.width,
+        thickness: data.thickness,
+        manufacturer: data.manufacturer,
+        spool_weight: data.spool_weight,
+        weight_10m: data.weight_10m,
+        reserve: data.reserve,
+        spool_qty: data.spool_qty,
+        weight: data.weight,
+        comment: data.comment,
+      });
+    }, () => {
+        this.showAlert('danger', 'Щось не так');
+    });
+  }
+
+  createMaterial() {
+    this.idEdit = null;
+    this.isNewMaterial = true;
+    this.reverseItemData.reset();
+  }
+
+  saveMaterial() {
+    this.idEdit = null;
+    const params = {
+      ...this.reverseItemData.value,
     };
-    ngOnInit(): void {
-        this._serviceMaterial.getListMaterial().subscribe((data: any) => {
-            this.reserveItems = data.sort((a, b) => a.name_color - b.name_color);
-
-            this.reserveItems.map(() => {
-                this.reverseItemsCorrect.push(
-                    this._fb.group({
-                        bab_quantity_color: null,
-                        weight_color: null,
-                    })
-                );
-            });
-        });
-
-        this.materialFilter = [
-            { id: 999, value: true, name: 'всі матеріали' },
-            { id: null, value: false, name: 'матеріали в наявності' },
-        ];
-
-        this.reverseItemData = this._fb.group({
-            id_color: '',
-            name_color: ['', Validators.required],
-            width_color: ['', Validators.required],
-            thickness_color: ['', Validators.required],
-            manufacturer_color: ['', Validators.required],
-            bab_weight_color: '',
-            weight_10m_color: ['', Validators.required],
-            reserve_color: ['', Validators.required],
-            bab_quantity_color: ['', Validators.required],
-            weight_color: ['', Validators.required],
-            comment_color: '',
-        });
-    }
-
-    editItem(id) {
-        this.idEdit = id;
-        this.isNewMaterial = false;
-        this._serviceMaterial.getFullInfoMaterial(id).subscribe((data: any) => {
-            this.reverseItemData.patchValue({
-                id_color: data.id_color,
-                name_color: data.name_color,
-                width_color: data.width_color,
-                thickness_color: data.thickness_color,
-                manufacturer_color: data.manufacturer_color,
-                bab_weight_color: data.bab_weight_color,
-                weight_10m_color: data.weight_10m_color,
-                reserve_color: data.reserve_color,
-                bab_quantity_color: data.bab_quantity_color,
-                weight_color: data.weight_color,
-                comment_color: data.comment_color,
-            });
-        });
-    }
-
-    createMaterial() {
-        this.idEdit = null;
-        this.isNewMaterial = true;
-        this.reverseItemData.reset();
-    }
-
-    saveMaterial() {
-        this.idEdit = null;
-        let params = {
-            ...this.reverseItemData.value,
-        };
-
-        delete params.id_color;
-        if (this.isNewMaterial) {
-            params = {
-                ...params,
-                color_new: 0,
-            };
-        } else {
-            params = {
-                ...params,
-                color_change_full: this.reverseItemData.value.id_color,
-            };
+    this.isShowSpinner = true;
+    this._serviceMaterial
+      .saveMaterial(params)
+      .pipe(
+        switchMap(() => {
+          return this._serviceMaterial.getListMaterial();
+        })
+      )
+      .subscribe(
+        (data: any[]) => {
+          this.isShowSpinner = false;
+          this.isShowSpinner = false;
+          this.prepareListMaterial(data);
+          this.reverseItemData.reset();
+        },
+        () => {
+          this.isShowSpinner = false;
         }
-        this.isShowSpinner = true;
+      );
+  }
 
-        this._serviceMaterial.saveMaterial(params).subscribe(() => {
-            this._serviceMaterial.getListMaterial().subscribe((data) => {
-                this.isShowSpinner = false;
-                this.reserveItems = data;
-                this.reverseItemData.reset();
-            }, () => {
-                this.isShowSpinner = false;
-            });
-        }, () => {
-            this.isShowSpinner = false;
-        });
-    }
-
-    getMaterialByFilter() {
-        this.isShowSpinner = true;
-        if (this.filterMaterial) {
-            this._serviceMaterial.getFullAllMaterial({ id_color: this.filterMaterial }).subscribe((data: any) => {
-                this.reserveItems = data.sort((a, b) => a.name_color - b.name_color);
-                this.isShowSpinner = false;
-                this.reserveItems.forEach(() => {
-                    this.reverseItemsCorrect.push(
-                        this._fb.group({
-                            bab_quantity_color: null,
-                            weight_color: null,
-                        })
-                    );
-                });
-            });
-        } else {
-            this._serviceMaterial.getListMaterial().subscribe((data: any) => {
-                this.reserveItems = data.sort((a, b) => a.name_color - b.name_color);
-                this.isShowSpinner = false;
-                this.reserveItems.forEach(() => {
-                    this.reverseItemsCorrect.push(
-                        this._fb.group({
-                            bab_quantity_color: null,
-                            weight_color: null,
-                        })
-                    );
-                });
-            });
+  editMaterial() {
+    const params = {
+      ...this.reverseItemData.value,
+    };
+    delete params.id_material;
+    this.isShowSpinner = true;
+    this._serviceMaterial
+      .editMaterial(params, this.reverseItemData.value.id_material)
+      .pipe(
+        switchMap(() => {
+          return this._serviceMaterial.getListMaterial();
+        })
+      )
+      .subscribe(
+        (data: any[]) => {
+          this.isShowSpinner = false;
+          this.prepareListMaterial(data);
+          this.isShowSpinner = false;
+          this.reverseItemData.reset();
+        },
+        () => {
+          this.isShowSpinner = false;
         }
-    }
-
-    calculateMat(form) {
-        form.patchValue({
-            weight_color: this.calculateString(form.value.weight_color),
-        });
-
-    }
-
-    calculateString(value) {
-        let total = 0;
-        value = value.toString().match(/[+\-]*(\.\d+|\d+(\.\d+)?)/g) || [];
-        while (value.length) {
-          total += parseFloat(value.shift());
-        }
-        return total;
+      );
+  }
+  getMaterialByFilter() {
+    this.isShowSpinner = true;
+    this._serviceMaterial.getListMaterial(this.filterMaterial ? { available: this.filterMaterial } : {}).subscribe(
+      (data: any) => {
+        this.prepareListMaterial(data);
+        this.isShowSpinner = false;
+      },
+      (err) => {
+        this.isShowSpinner = false;
+        this.reserveItems = [];
+        this.showAlert('danger', err.error.materials);
       }
+    );
+  }
 
-    changeMaterial(item, value, field, id) {
-        this.idChange.push(id);
-        this.isHideOk = false;
-        item.patchValue({
-            [field]: value,
+  calculateMat(form) {
+    form.patchValue({
+      weight: this.calculateWeight(form.value.weight),
+    });
+  }
+
+  calculateWeight(value) {
+    let total = 0;
+    value = value.toString().match(/[+\-]*(\.\d+|\d+(\.\d+)?)/g) || [];
+    while (value.length) {
+      total += parseFloat(value.shift());
+    }
+    return total;
+  }
+
+  changeMaterial(item, value, field, id) {
+    this.idChange.push(id);
+    this.isHideOk = false;
+    item.patchValue({
+      [field]: +value,
+    });
+  }
+
+  saveChangesMat(id, item) {
+    this.isShowSpinner = true;
+    this._serviceMaterial.saveConsumptionMaterial(item.value, id).subscribe(
+      (data: any) => {
+        this.isShowSpinner = false;
+        this.idChange = this.idChange.filter((i) => i !== id);
+        this.reserveItems.forEach((item) => {
+          if (item.id_material === id) {
+            item.net_weight = data.net_weight;
+            item.spool_qty = data.spool_qty;
+          }
         });
-    }
+        this.reverseItemData.patchValue({
+          edit_spool_qty: this.reverseItemData.value.edit_spool_qty + item.value.edit_spool_qty,
+          edit_weight: this.reverseItemData.value.edit_weight + item.value.edit_weight,
+        });
+        this.isShowSpinner = false;
 
-    saveChangesMat(id, item) {
-        this.isShowSpinner = true;
-        const params = {
-            color_change: id,
-            bab_quantity_color: +item.value.bab_quantity_color,
-            weight_color: +item.value.weight_color,
-        };
-        this._serviceMaterial
-            .saveMaterial(params)
-            .pipe(switchMap(() => this._serviceMaterial.getListMaterial()))
-            .subscribe((data: any) => {
-                this.isShowSpinner = false;
-                this.idChange = this.idChange.filter((i) => i !== id);
-                this.reserveItems = data.sort((a, b) => a.name_color - b.name_color);
-                this.reverseItemData.patchValue({
-                    bab_quantity_color: this.reverseItemData.value.bab_quantity_color + params.bab_quantity_color,
-                    weight_color: this.reverseItemData.value.weight_color + params.weight_color,
-                });
-                this.isShowSpinner = false;
-                this.alert = {
-                    isShow: true,
-                    type: 'success',
-                    message: 'Дані збережено',
-                };
-                setTimeout(() => {
-                    this.alertChange(false);
-                }, 3000);
-                item.reset();
-            }, () => {
-                this.isShowSpinner = false;
-                this.alert = {
-                    isShow: true,
-                    type: 'danger',
-                    message: 'Уппс, щось пішло не так',
-                };
-                setTimeout(() => {
-                    this.alertChange(false);
-                }, 3000);
-            });
-    }
+        this.showAlert('success',  'Дані збережено');
+        item.reset();
+      },
+      () => {
+        this.isShowSpinner = false;
+        this.showAlert('danger', 'Уппс, щось пішло не так');
+      }
+    );
+  }
 
-    alertChange(e) {
-        this.alert.isShow = e;
-    }
+  showAlert(type, message) {
+    this.alert = {
+        isShow: true,
+        type: type,
+        message: message,
+      };
+      setTimeout(() => {
+        this.alertChange(false);
+      }, 3000);
+  }
+
+  alertChange(e) {
+    this.alert.isShow = e;
+  }
 }
--- frontend/src/app/services/materials.service.ts
+++ frontend/src/app/services/materials.service.ts
@@ -4,26 +4,27 @@ import { HttpClient } from '@angular/common/http';
     providedIn: 'root',
 })
 export class MaterialPageService {
-    url = `${this._apiUrl}/material`;
+    url = `${this._apiUrl}/materials`;
 
     constructor(private _http: HttpClient, @Inject('API_URL') private _apiUrl: string) {}
 
-    getListMaterial() {
-        return this._http.get(this.url);
+    getListMaterial(params?) {
+        return this._http.get(this.url, { params });
     }
 
     getFullInfoMaterial(id) {
-        const params = {
-            id_color: id,
-        };
-        return this._http.post(this.url, params);
-    }
-
-    getFullAllMaterial(params) {
-        return this._http.post(this.url, params);
+        return this._http.get(`${this.url}/${id}`);
     }
 
     saveMaterial(data) {
         return this._http.post(this.url, data);
+    }
+
+    saveConsumptionMaterial(params, id) {
+        return this._http.put(`${this.url}/consumption/${id}`, params);
+    }
+
+    editMaterial(data, id) {
+        return this._http.put(`${this.url}/${id}`, data);
     }
 }
