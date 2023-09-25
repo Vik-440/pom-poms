4691782f73563da48d0e0f8a6ce83faae6113066
DanaDov <danadovzh16@gmail.com>
2023-09-25 21:42:24 +0300

    fix lint

--- frontend/src/app/create-order/create-order.component.ts
+++ frontend/src/app/create-order/create-order.component.ts
@@ -750,10 +750,10 @@ export class CreateOrderComponent implements OnInit {
     this.orderForm.value.forEach((order, i) => {
       const type = modelsData[order.article.substring(0, 3)] || modelsData[order.article.substring(0, 2)] || null;
       copyText.push(
-        `${i + 1}. ${type},` 
-        + `колір ${order.colors},` 
-        + `код ${order.article},` 
-        + `кількість ${this.orderAddForm.controls[i].value.qty_pars || 0} ${type.includes('брелок') ? 'шт' : 'пар'},` 
+        `${i + 1}. ${type}, ` 
+        + `колір ${order.colors.trim()}, ` 
+        + `код ${order.article}, ` 
+        + `кількість ${this.orderAddForm.controls[i].value.qty_pars || 0} ${type.includes('брелок') ? 'шт' : 'пар'}, ` 
         + `ціна ${formatNumber(order.price)} грн/${type.includes('брелок') ? 'шт' : 'пара'}\n`
       );
     });
